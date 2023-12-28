9841dec1b149945bdef70ae3ccef5e86805a6015
Jayden <jduncan9@stu.jsu.edu>
2023-12-27 19:32:07 -0600

    Initial Commit

---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
+import { RecipesService } from './recipes.service';
+
+@Component({
+  selector: 'app-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.scss']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor(private recipesService: RecipesService) { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from "@angular/core";
+import { Recipe } from "./recipe.model";
+import { Observable } from "rxjs";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RecipesService {
+    
+}
